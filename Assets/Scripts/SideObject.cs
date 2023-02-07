b7f90d24da1249cc90e83391369aa7cbf0b042db
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2023-02-07 16:32:13 +0000

    AA-83 task boundary scrolling complete

---
+++ Assets/Scripts/SideObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideObject : MonoBehaviour
+{
+    private SideObjectSpawner _sideObjectSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sideObjectSpawner = GameObject.FindObjectOfType<SideObjectSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _sideObjectSpawner.SpawnTile();
+            Destroy(gameObject, 2);
+        }
+    }
+}
