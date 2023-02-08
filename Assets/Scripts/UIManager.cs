0e65011ea91ab43670f55ae8e9be414cbde084dc
Izabela-Zelek <C00247865@itcarlow.ie>
2023-02-08 12:53:14 +0000

    Completed Connect form to button - AA-54

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// REMOVE BEFORE COMMIT
+    /// </summary>
+    public void GoToFormRatingForm()
+    {
+        Application.OpenURL(
+            "https://docs.google.com/forms/d/e/1FAIpQLScP_TMz7UApxi89AMviheraT5H10M79u2_8E2bpfZZvgiWpZw/viewform?usp=sf_link");
+    }
+}
