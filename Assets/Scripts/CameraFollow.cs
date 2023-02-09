9767a7a2ec290c01fdf095c656a06e1cfeda2357
Adrien Dudon <dudonadrien@gmail.com>
2023-02-09 11:15:10 +0000

    Basic camera script for the God that smoothly follow the player. - AA-91

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,21 +1,42 @@
+using Mirror.Examples.SyncDir;
 using UnityEngine;
+
+internal enum CameraType {
+    Runner,
+    God
+}
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform _player;
-    Vector3 _offset;
+    [SerializeField] private CameraType type = CameraType.Runner;
+    [SerializeField] private Transform runner;
 
-    // Start is called before the first frame update
+    [Header("God Camera Settings (only if type is God)")]
+    [SerializeField] private float distanceFromGod = 10;
+    [SerializeField] private float heightFromGod = 25;
+
+    private Vector3 _offset;
+    private Vector3 _currentVelocity;
+
     void Start()
     {
-        _offset = transform.position - _player.position;
+        if (type == CameraType.Runner)
+            _offset = transform.position - runner.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        Vector3 newPos = _player.position + _offset;
-
-        transform.position = new Vector3(transform.position.x, transform.position.y, newPos.z);
+        if (type == CameraType.Runner)
+        {
+            Vector3 newPos = runner.position + _offset;
+            transform.position = new Vector3(transform.position.x, transform.position.y, newPos.z);
+        }
+        else if (type == CameraType.God)
+        {
+            Vector3 targetPos = runner.transform.position + (-runner.transform.forward * distanceFromGod);
+            targetPos += Vector3.up * heightFromGod;
+            transform.position = targetPos;
+            transform.LookAt(runner);
+        }
     }
 }
