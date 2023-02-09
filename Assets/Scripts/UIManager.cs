7deb515752ce7a4a5586186c2bfd22a372b1ea86
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2023-02-09 14:48:12 +0000

    Switching To Ai Complete - AA-75
    commit 0996f387a4ca27a00fe6ac8ae6cd09ef38c81c6a
    Author: Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
    Date:   Thu Feb 9 14:46:04 2023 +0000
    
        task part 2/2 complete. Added code to toggle ai on and off during gameplay
    
    commit a59f577649c4f0ca2a3c6619959940766c6accf3
    Author: Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
    Date:   Thu Feb 9 14:04:59 2023 +0000
    
        task part 1 add button and ui manager to handle switching between AI and player

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,15 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject _runner;
+    public Button _toggleAIButton;
+    bool _aiOnOff;
+
+    private void Start()
+    {
+        //_toggleAIButton = _gameplayCanvas.GetComponentInChildren<Button>();
+    }
+
     /// <summary>
     /// REMOVE BEFORE COMMIT
     /// </summary>
+    /// 
     public void GoToFormRatingForm()
     {
         Application.OpenURL(
             "https://docs.google.com/forms/d/e/1FAIpQLScP_TMz7UApxi89AMviheraT5H10M79u2_8E2bpfZZvgiWpZw/viewform?usp=sf_link");
     }
+
+    public void ToggleAIButton()
+    {
+        // _toggleAIButton.text = "Hi";
+        if (!_aiOnOff)
+        {
+            _aiOnOff = true;
+            _toggleAIButton.image.color = Color.red;
+            _runner.transform.GetChild(1).gameObject.SetActive(true);
+            _runner.GetComponent<AI_Brain>().enabled = true;
+            _runner.GetComponent<RunnerPlayer>().enabled = false;
+
+        }
+        else
+        {
+            _aiOnOff = false;
+            _toggleAIButton.image.color = Color.white;
+            _runner.transform.GetChild(1).gameObject.SetActive(false);
+            _runner.GetComponent<AI_Brain>().enabled = false;
+            _runner.GetComponent<RunnerPlayer>().enabled = true;
+
+
+        }
+    }
+
+
 }
