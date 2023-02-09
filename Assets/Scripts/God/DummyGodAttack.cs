9f850ae32dbe5b8bac429122a8f8d9d9d28dfb7e
Adrien <dudonadrien@gmail.com>
2023-02-09 17:35:01 +0000

    Actual logic that select a random attack if the God is selected to be an AI - AA-77

---
+++ Assets/Scripts/God/DummyGodAttack.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// Just an example script to show how to create a new attack.
+/// </summary>
+public class DummyGodAttack : GodAttack
+{
+    /// <summary>
+    /// Don't forget to set the attack type in the Start method.
+    /// </summary>
+    private void Start()
+    {
+        attackType = GodAttackType.Dummy;
+    }
+
+    /// <summary>
+    /// Just an example method to show how to create a new attack. You should put the attack logic here in real code.
+    /// </summary>
+    public override void Attack()
+    {
+        Debug.Log("God attack with 'Dummy'!");
+    }
+}
