cc45380c02f49da90e744fb941d1795ea1cb7723
Danial <danialhakim01@gmail.com>
2023-02-10 13:22:54 +0000

    Completed Spawning Of Obstacle AA-71
    Create scene to show player attacks

---
+++ Assets/Scripts/SlideGodAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideGodAttack : GodAttack
+{
+    private GameObject _avoidThunder;
+
+    /// <summary>
+    /// Sets attack type to Avoid and loads avoid gameobject prefab
+    /// </summary>
+    private void Start()
+    {
+        attackType = GodAttackType.Slide;
+        _avoidThunder = Resources.Load("AvoidThunder") as GameObject;
+    }
+
+    /// <summary>
+    /// Spawns the avoid gameobject prefab at given target position
+    /// </summary>
+    /// <param name="targetPos"></param>
+    public override void Attack(Vector3 targetPos)
+    {
+        Quaternion rotation = Quaternion.Euler(90, 90, 0);
+        Instantiate(_avoidThunder, targetPos, rotation);
+        //Debug.Log("Spawned");
+    }
+
+    public override void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
