ad0787de17b0f8eab7792f4ac7b7c99f7ed8b6e6
Danial <danialhakim01@gmail.com>
2023-02-13 14:39:23 +0000

    Completed Attacks only placed on lane AA-151

--- Assets/Scripts/God/GodPlayer.cs
+++ Assets/Scripts/God/GodPlayer.cs
@@ -12,63 +12,65 @@ public class GodPlayer : MonoBehaviour
     private bool _canAttack = true;
     private AIGod _ai;
     private GodAttack _activeAttack;
+    int chosenAttack = -1;
 
     private PlayerGodAttackList _playerAttack;
     private Vector3 _attackPos = new Vector3();
+
+    public float distance = 10f;
+
+    public LayerMask mask;
 
     private void Start()
     {
         _ai = GetComponent<AIGod>();
         _playerAttack = GameObject.Find("God Canvas").GetComponent<PlayerGodAttackList>();
-        //_uIGameManager = GameObject.Find("UIGameManager").GetComponent<UIGameManager>();
     }
 
     /// <summary>
     /// if is an AI, calls Attack
     /// If not an AI and detects touch, grabs and converts the position to world space
     /// </summary>
-    //private void Update()
-    //{
-    //    if (isAI && _canAttack)
-    //    {
-    //        Attack();
-    //    }
+    private void Update()
+    {
+        if (isAI && _canAttack)
+        {
+            Attack();
+        }
 
-    //    if (!isAI && _canAttack)
-    //    {
-    //        if (Input.GetMouseButtonDown(0) && _uIGameManager.GetChosenAttack() != -1)
-    //        {
-    //            _activeAttack = _playerAttack.GetAttack(_uIGameManager.GetChosenAttack());
-    //            Vector3 pos = Input.mousePosition;
-    //            pos.z = Camera.main.nearClipPlane + 30;
-    //            pos = Camera.main.ScreenToWorldPoint(pos);
-    //            if(_uIGameManager.GetChosenAttack() == 1)
-    //            {
-    //                pos.y = 3;
-    //            }
-    //            else
-    //            {
-    //                pos.y = 2;
-    //            }
-    //            _attackPos = pos;
-    //            PlayerAttack();
-    //        }
-    //        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && _uIGameManager.GetChosenAttack() != -1)
-    //        {
-    //            _activeAttack = _playerAttack.GetAttack(_uIGameManager.GetChosenAttack());
-    //            Vector3 pos = Input.GetTouch(0).position;
-    //            pos.z = Camera.main.nearClipPlane + 30;
-    //            pos = Camera.main.ScreenToWorldPoint(pos);
-    //            pos.y = 2;
-    //            _attackPos = pos;
-    //            PlayerAttack();
-    //        }
-    //    }
-    //}
+        if (!isAI && _canAttack)
+        {
+            if ((Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) && chosenAttack != -1)
+            {
+                if (PlacementAllow())
+                {
+                    _activeAttack = _playerAttack.GetAttack(chosenAttack);
+                    Vector3 pos = Input.mousePosition;
+                    pos.z = Camera.main.nearClipPlane + 30;
+                    pos = Camera.main.ScreenToWorldPoint(pos);
+                    if (chosenAttack == 1)
+                    {
+                        pos.y = 3;
+                    }
+                    else
+                    {
+                        pos.y = 2;
+                    }
+                    _attackPos = pos;
+                    PlayerAttack();
+                }
+                else
+                {
+                    Debug.Log("pick another place");
+                }
+            }
+        }
+    }
 
-    private void ChooseAttack(int t_attack)
+    public void ChooseAttack(int t_attack)
     {
         _activeAttack = _playerAttack.GetAttack(t_attack);
+        chosenAttack = t_attack;
     }
 
     /// <summary>
@@ -111,5 +113,23 @@ public class GodPlayer : MonoBehaviour
             _activeAttack.Attack(_attackPos);
             StartCoroutine(StartAttackCooldown());
         }
+    }
+
+    private bool PlacementAllow()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        // Check if the raycast hits a specific GameObject
+        if (Physics.Raycast(ray, out hitInfo,Mathf.Infinity,mask))
+        {
+            Debug.Log(hitInfo.collider.gameObject.tag);
+            GameObject hitObject = hitInfo.collider.gameObject;
+            if (hitObject.tag == "Ground")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
