1a0a5640f1f3b63f09a986ce72b0bb07fa90a4e3
Izabela-Zelek <C00247865@itcarlow.ie>
2023-02-14 23:58:14 +0000

    Completed Create Goals - AA-149

--- Assets/Scripts/CollectibleController.cs
+++ Assets/Scripts/CollectibleController.cs
@@ -24,18 +24,32 @@ public class CollectibleController : MonoBehaviour
             if(gameObject.transform.tag == "SpeedUp")
             {
                 GameObject.Find("PickupController").GetComponent<PickupController>().SpeedUp();
+                GameObject.Find("PickupController").GetComponent<GoalController>().AddSpeedUp();
             }
             else if (gameObject.transform.tag == "SpeedDown")
             {
                 GameObject.Find("PickupController").GetComponent<PickupController>().SpeedDown();
+                GameObject.Find("PickupController").GetComponent<GoalController>().AddSpeedDown();
             }
             else if (gameObject.transform.tag == "MaxHealth")
             {
+                if(GameObject.Find("RunnerPlayer").GetComponent<RunnerHealthController>().GetHealth() == GameObject.Find("RunnerPlayer").GetComponent<RunnerHealthController>().GetMaxHealth())
+                {
+                    GameObject.Find("PickupController").GetComponent<GoalController>().CheckHealth();
+                }
                 GameObject.Find("RunnerPlayer").GetComponent<RunnerHealthController>().InstaHeal();
             }
             else if (gameObject.transform.tag == "PartHealth")
             {
                 GameObject.Find("RunnerPlayer").GetComponent<RunnerHealthController>().PartialHeal();
+            }
+            else if (gameObject.transform.tag == "Apple")
+            {
+                GameObject.Find("PickupController").GetComponent<GoalController>().AddApple();
+            }
+            else if (gameObject.transform.tag == "Pomegranate")
+            {
+                GameObject.Find("PickupController").GetComponent<GoalController>().AddPom();
             }
             Destroy(gameObject);
         }
