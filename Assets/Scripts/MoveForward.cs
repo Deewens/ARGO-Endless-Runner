5ce219d82c6d04550c801245642328576d1f2721
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2023-02-16 02:52:46 +0000

    AA-153 task Bonus Points for distance, playtime and speed complete. Ui added to display achievements

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -1,3 +1,6 @@
+/// Author : Patrick Donnelly
+/// Contributors : ---
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,24 +11,31 @@ using UnityEngine.Analytics;
 /// </summary>
 public class MoveForward : MonoBehaviour
 {
-    public float _speed;
-    public float _maxSpeed;
+    private double _startTime;
+    private double _endTime;
+    private double _playTime;
+    private int _speed;
+    private const int MaxSpeed = 30;
     public Rigidbody _rb;
-    public int _distanceTravelled;
+    private int _distanceTravelled;
     int _startingPos;
     int _increaseWhen;
     public int _obstaclesAvoided;
+    private bool _isSpeedIncreased;
 
     /// <summary>
     /// Sets up default values
     /// </summary>
     void Start()
     {
+        _endTime = 0;
+        _playTime = 0;
+        _startTime = System.DateTimeOffset.Now.ToUnixTimeSeconds();
         _increaseWhen = 100;
-        _speed = 8.0f;
-        _maxSpeed = 30.0f;
+        _speed = 8;
         _startingPos = (int)transform.position.z;
-        _obstaclesAvoided= 0;
+        _obstaclesAvoided = 0;
+        _isSpeedIncreased = false;
     }
 
 
@@ -34,23 +44,55 @@ public class MoveForward : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-
         _distanceTravelled = (int)transform.position.z - (int)_startingPos;
 
         if (_distanceTravelled % _increaseWhen == 0 && _distanceTravelled != 0)
         {
-            if (_speed < _maxSpeed)
+            if (_speed < MaxSpeed)
             {
-                _speed += 2.0f;
+                _speed += 2;
                 _increaseWhen += 100;
+                _isSpeedIncreased = true;
             }
             else
             {
-                _speed = _maxSpeed;
+                _speed = MaxSpeed;
             }
         }
 
         Vector3 _moveForward = transform.forward * _speed * Time.fixedDeltaTime;
         _rb.MovePosition(_rb.position + _moveForward);
+    }
+
+    public int GetDistanceTravelled()
+    {
+        return _distanceTravelled;
+    }
+
+    public int GetSpeed()
+    {
+        return _speed;
+    }
+
+    public int GetMaxSpeed()
+    {
+        return MaxSpeed;
+    }
+
+    public bool GetIsSpeedIncreased()
+    {
+        return _isSpeedIncreased;
+    }
+
+    public void SetIsSpeedIncreased(bool IsSpeedIncreased)
+    {
+        _isSpeedIncreased = IsSpeedIncreased;
+    }
+
+    public int GetPlayTime()
+    {
+        _endTime = System.DateTimeOffset.Now.ToUnixTimeSeconds();
+        _playTime = _endTime - _startTime;
+        return (int)_playTime;
     }
 }
