d4ac946784c1cf3804b74545bb42169c452f2c1f
Patrick Donnelly <45255674+PatrickDonnelly@users.noreply.github.com>
2023-02-17 11:36:38 +0000

    Added the new assets to the main scene and adjusted the runner reference script to accomodate them

--- Assets/Scripts/Misc/LightScroller.cs
+++ Assets/Scripts/Misc/LightScroller.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BGScroller : MonoBehaviour
+public class LightScroller : MonoBehaviour
 {
     private Transform _runner;
 
