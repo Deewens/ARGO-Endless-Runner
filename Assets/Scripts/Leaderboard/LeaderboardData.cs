4b1b4465e39b15c61784692e4e0a39b29ed6b771
Adrien <dudonadrien@gmail.com>
2023-02-17 13:51:20 +0000

    AA-131 - Create the UI for the leaderboard and fill it with fake data

---
+++ Assets/Scripts/Leaderboard/LeaderboardData.cs
@@ -0,0 +1,15 @@
+﻿public struct LeaderboardData
+{
+    public LeaderboardData(int no, string username, int bestRunnerScore, int bestGodScore)
+    {
+        this.no = no;
+        this.username = username;
+        this.bestRunnerScore = bestRunnerScore;
+        this.bestGodScore = bestGodScore;
+    }
+
+    public int no;
+    public string username;
+    public int bestRunnerScore;
+    public int bestGodScore;
+}
