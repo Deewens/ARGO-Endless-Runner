4982c4539910828c0ff610923a8678693eed8419
Adrien <dudonadrien@gmail.com>
2023-02-28 11:42:00 +0000

    Add input to manually type host IP address in the menu

--- Assets/Scripts/MultiplayerMenu.cs
+++ Assets/Scripts/MultiplayerMenu.cs
@@ -1,6 +1,8 @@
 ﻿/*
 Olympus Run - A game made as part of the ARGO Project at SETU Carlow
-Copyright (C) 2023 Caroline Percy <lineypercy@me.com>, Patrick Donnelly <patrickdonnelly3759@gmail.com>, Izabela Zelek <izabelawzelek@gmail.com>, Danial-hakim <danialhakim01@gmail.com>, Adrien Dudon <dudonadrien@gmail.com>
+Copyright (C) 2023 Caroline Percy <lineypercy@me.com>, Patrick Donnelly <patrickdonnelly3759@gmail.com>, 
+                   Izabela Zelek <izabelawzelek@gmail.com>, Danial-hakim <danialhakim01@gmail.com>, 
+                   Adrien Dudon <dudonadrien@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,7 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+using System;
 using Mirror;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -26,7 +30,9 @@ public class MultiplayerMenu : MonoBehaviour
     [Scene] 
     [SerializeField] 
     private string mainMenuScene = "";
-    
+
+    [SerializeField] private TMP_InputField _ipInputField;
+
     public void LoadMainMenuScene()
     {
         SceneManager.LoadScene(mainMenuScene);
@@ -39,6 +45,7 @@ public class MultiplayerMenu : MonoBehaviour
     
     public void StartClientButton()
     {
-        NetworkManager.singleton.StartClient();
+        var uri = new Uri($"kcp://{_ipInputField.text}:7777");
+        NetworkManager.singleton.StartClient(uri);
     }
 }
